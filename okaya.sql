005cda392e12e5984387c8a689c32cd5dc4633a0
tanuj shankarwar <60888384+tanuj312001@users.noreply.github.com>
2023-05-19 17:38:37 +0530

    Add files via upload

---
+++ okaya.sql
@@ -0,0 +1,4 @@
+#Write a SQL query that returns the name, phone number and most recent date
+# for any user that has logged in over the last 30 days 
+#(you can tell a user has logged in if the action field in UserHistory is set to "logged_on").
+SELECT * FROM userhistory
